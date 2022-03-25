4b383e50834027a86c8fee2e52bbc795e221b5a9
azat valiev <azvaliev@gmail.com>
2022-03-24 17:16:27 -0700

    fixing confirm password

---
+++ components/Logic/signup/checkConfirmPassword.ts
@@ -0,0 +1,8 @@
+const checkConfirmPassword = (password: string, confirmPassword: string) => {
+	if (password === confirmPassword) {
+		return 3;
+	}
+	return 1;
+};
+
+export default checkConfirmPassword;
