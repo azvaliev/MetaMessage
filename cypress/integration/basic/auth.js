50abbbad87eb3e3032bdf2b5f3ee7884f930242f
azat valiev <azvaliev@gmail.com>
2022-03-24 18:23:16 -0700

    Added new E2E test. Check for correct message home

--- cypress/integration/basic/auth.js
+++ cypress/integration/basic/auth.js
@@ -1,56 +1,73 @@
-const BobPassword = '$QD$Yi%s@VY3';
-const AlicePassword = 'IQAYh^$Sr5YY';
+/* eslint-disable no-undef */
+const BobPassword = "$QD$Yi%s@VY3";
+const AlicePassword = "IQAYh^$Sr5YY";
 
-describe('Create Account', () => {
-    it('Loads Welcome Page', () => {
-        cy.visit('http://localhost:3000');
-        cy.contains('sign up', { matchCase: false });
-    });
-    it('Can Access Signup Page', () => {
-        cy.get('button').click();
-        cy.contains('password', { matchCase: false });
-        cy.url().should('include', '/signup');
-    });
-    it('Can Signup', () => {
-        cy.get('input[name=password]').type(BobPassword);
-        cy.get('input[name=confirmPassword]').type(BobPassword);
-        cy.get('button').click();
-        cy.wait(250);
-        cy.url().should('not.have', '/signup');
-    })
-})
-describe('Account Actions', () => {
-    it('Can Access & Copy Address', () => {
-        cy.get('h1').click();
-        cy.wait(250);
-        cy.url().should('include', '/profile');
-    });
-    it('Can Overwrite Account', () => {
-        // Visit signup page again
-        cy.visit('http://localhost:3000/welcome')
-        cy.contains('sign up', { matchCase: false })
-        cy.get('button').click();
-        cy.get('input[name=password]').type(AlicePassword);
-        cy.get('input[name=confirmPassword]').type(AlicePassword);
-        cy.get('button').click();
-        cy.wait(250);
-        cy.url().should('not.have', '/signup')
-    });
-    it('Can Logout', () => {
-        cy.get('h1').click();
-        cy.get('#logout').click();
-        cy.wait(250);
-        cy.url().should('include', '/login');
-    })
-    it('Can Delete Account', () => {
-        cy.visit('http://localhost:3000/signup')
-        cy.get('input[name=password]').type(AlicePassword);
-        cy.get('input[name=confirmPassword]').type(AlicePassword); 
-        cy.get('button').click();
-        cy.wait(250); 
-        cy.get('h1').click();
-        cy.get('#deleteAccount').click();
-        cy.wait(250);
-        cy.url().should('include', '/welcome');
-    })
-})
+describe("Create Account", () => {
+	it("Loads Welcome Page", () => {
+		cy.visit("http://localhost:3000");
+		cy.contains("sign up", { matchCase: false });
+	});
+	it("Can Access Signup Page", () => {
+		cy.get("button").click();
+		cy.contains("password", { matchCase: false });
+		cy.url().should("include", "/signup");
+	});
+	it("Can Signup", () => {
+		cy.get("input[name=password]").type(BobPassword);
+		cy.get("input[name=confirmPassword]").type(BobPassword);
+		cy.get("button").click();
+		cy.wait(250);
+		cy.url().should("not.have", "/signup");
+	});
+});
+describe("Displays no conversations yet", () => {
+	it("Signs up for an account", () => {
+		cy.visit("http://localhost:3000/welcome");
+		cy.contains("sign up", { matchCase: false });
+		cy.get("button").click();
+		cy.get("input[name=password]").type(AlicePassword);
+		cy.get("input[name=confirmPassword]").type(AlicePassword);
+		cy.get("button").click();
+		cy.wait(250);
+		cy.url().should("not.have", "/signup");
+	});
+	it("Displays the proper message", () => {
+		cy.wait(3000);
+		cy.contains("No conversations yet");
+	});
+});
+describe("Account Actions", () => {
+	it("Can Access & Copy Address", () => {
+		cy.get("h1").click();
+		cy.wait(250);
+		cy.url().should("include", "/profile");
+	});
+	it("Can Overwrite Account", () => {
+		// Visit signup page again
+		cy.visit("http://localhost:3000/welcome");
+		cy.contains("sign up", { matchCase: false });
+		cy.get("button").click();
+		cy.get("input[name=password]").type(AlicePassword);
+		cy.get("input[name=confirmPassword]").type(AlicePassword);
+		cy.get("button").click();
+		cy.wait(250);
+		cy.url().should("not.have", "/signup");
+	});
+	it("Can Logout", () => {
+		cy.get("h1").click();
+		cy.get("#logout").click();
+		cy.wait(250);
+		cy.url().should("include", "/login");
+	});
+	it("Can Delete Account", () => {
+		cy.visit("http://localhost:3000/signup");
+		cy.get("input[name=password]").type(AlicePassword);
+		cy.get("input[name=confirmPassword]").type(AlicePassword); 
+		cy.get("button").click();
+		cy.wait(250); 
+		cy.get("h1").click();
+		cy.get("#deleteAccount").click();
+		cy.wait(250);
+		cy.url().should("include", "/welcome");
+	});
+});
